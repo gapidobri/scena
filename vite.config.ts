6d965720a8e02830474b8300ff74c6f5526fc7f0
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2023-03-09 21:59:02 +0100

    Re-added prisma browser fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { createRequire } from 'module';
+import path from 'path';
 import type { UserConfig } from 'vite';
+
+const require = createRequire(import.meta.url);
+
+const prismaClient = require
+	.resolve('@prisma/client')
+	.replace(/@prisma(\/|\\)client(\/|\\)index\.js/, '.prisma/client/index-browser.js');
+
+const prismaIndexBrowser = path.normalize(path.relative(process.cwd(), prismaClient));
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
+	resolve: { alias: { '.prisma/client/index-browser': prismaIndexBrowser } },
 };
 
 export default config;
