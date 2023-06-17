ad030b3ae1b6613f931253b2ae9655ac9147ad11
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2023-06-17 21:34:00 +0200

    Pretty errors

---
+++ src/routes/dash/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { faFrown } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa/src/fa.svelte';
+</script>
+
+<div class="flex flex-col items-center justify-center h-1/2">
+	<span class="text-2xl mb-10">{$page.error?.message}</span>
+	<Fa icon={faFrown} scale={4} />
+</div>
